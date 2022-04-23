81ce9cbb5e2e4c4fd56449fbb184ffca83d8ef59
anttimc <makinen.antti.j@gmail.com>
2022-04-23 17:18:14 +0300

    Add first pybricks script
    -timeout does not work yet

---
+++ mvp_test.py
@@ -0,0 +1,41 @@
+"""
+This program is for MVP's basic "Buggy" mode.
+Follow the corresponding building instructions in the LEGO® MINDSTORMS®
+Robot Inventor App.
+"""
+
+from pybricks.pupdevices import Motor, UltrasonicSensor
+from pybricks.parameters import Direction, Port
+from pybricks.tools import wait, StopWatch
+
+
+class MVP:
+    def __init__(self):
+        self.steer_motor = Motor(Port.A)
+        self.drive_motor = Motor(Port.B,
+                                 positive_direction=Direction.COUNTERCLOCKWISE)
+
+    def calibrate(self):
+        self.steer_motor.run_target(speed=1000, target_angle=-20)
+
+
+# Initialize mvp and straighten its steering.
+mvp = MVP()
+mvp.calibrate()
+
+distance_sensor = UltrasonicSensor(Port.E)
+
+# Make mvp drive in a circle.
+mvp.steer_motor.run_angle(speed=350, rotation_angle=-50)
+mvp.drive_motor.run(speed=800) #, rotation_angle=32 * 360)
+
+timer = StopWatch()
+timeout = 30 * 1000 # ms
+
+while timer.time() < timeout:
+    if distance_sensor.distance() < 100:
+        distance_sensor.lights.on(100)
+        mvp.drive_motor.stop()
+    else:
+        distance_sensor.lights.off()
+        mvp.drive_motor.run(speed=800)
---
+++ test_pybricks.py
@@ -0,0 +1,23 @@
+import pybricksdev as pbd
+from pybricksdev.connections.pybricks import PybricksHub
+from pybricksdev.ble import find_device #, nus
+import logging
+import sys
+import asyncio
+
+async def main():
+    address = await find_device('Pybricks Hub')
+    print(address)
+
+    hub = PybricksHub()
+    await hub.connect(address)
+    print('hub connected:', hub.connected)
+    
+    
+    await hub.run('mvp_test.py')
+    
+    await hub.disconnect()
+
+if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    asyncio.run(main())
